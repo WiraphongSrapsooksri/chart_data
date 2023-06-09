6e6a5a30e31101a1eb255579422a9a2a35ddd098
Wiraphong2003 <111116563+Wiraphong2003@users.noreply.github.com>
2023-06-10 00:57:56 +0700

    as

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const datamajor = require('./dataMajor.json')
+app.get("/", (req, res) => {
+   res.send("Hello! Node.js");
+});
+
+app.get('/datamajor', (req, res) => {
+   res.json(datamajor)
+})
+
+// app.get('/datamajor/:code', (req, res) => {
+//    res.json(datamajor.find(data =>
+//       console.log(data)
+//       // data => data.code === Number(req.params.code)
+//       ))
+// })
+
+app.listen(port, () => {
+   console.log("Starting node.js at port " + port);
+});
