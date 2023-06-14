ac443f9acdffefeda044542bc1523a97e7f71d0d
Wiraphong2003 <111116563+Wiraphong2003@users.noreply.github.com>
2023-06-15 00:45:30 +0700

    as

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
+// const express = require("express");
+// const app = express();
+// const port = process.env.PORT || 3000;
+// const datamajor = require('./dataMajor.json')
+// const datas = require("./data2.json")
+
+
+// app.get("/", (req, res) => {
+//    res.send("Hello! Node.js");
+// });
+
+// app.get('/datamajor', (req, res) => {
+//    res.json(datas)
+// })
+// app.get('/Group/:id', function (req, res) {
+//    res.send(req.params.id);
+// });
+
+// app.listen(port, () => {
+//    console.log("Starting node.js at port " + port);
+// });
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const datamajor = require('./dataMajor.json')
-const datas = require("./data2.json")
-
+const datas = require("./data2.json");
 
 app.get("/", (req, res) => {
-   res.send("Hello! Node.js");
+   res.send("DATA REG");
 });
 
 app.get('/datamajor', (req, res) => {
-   res.json(datas)
-})
-app.get('/Group/:id', function (req, res) {
-   res.send('Group' + req.params.id);
+   res.json(datas);
+});
+
+app.get('/Group/:id', (req, res) => {
+   const groupId = req.params.id;
+   const groupData = datas.filter(data => data.group === groupId);
+
+   if (groupData.length > 0) {
+      res.json(groupData);
+   } else {
+      res.status(404).send("Group not found");
+   }
 });
 
 app.listen(port, () => {
