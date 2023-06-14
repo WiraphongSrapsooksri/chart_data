30eaaeff3858a74d677a7d0a8a0fec8549a659d8
Wiraphong2003 <111116563+Wiraphong2003@users.noreply.github.com>
2023-06-15 03:17:13 +0700

    Filter and data

--- index.js
+++ index.js
@@ -1,47 +1,67 @@
-// const express = require("express");
-// const app = express();
-// const port = process.env.PORT || 3000;
-// const datamajor = require('./dataMajor.json')
-// const datas = require("./data2.json")
 
-
-// app.get("/", (req, res) => {
-//    res.send("Hello! Node.js");
-// });
-
-// app.get('/datamajor', (req, res) => {
-//    res.json(datas)
-// })
-// app.get('/Group/:id', function (req, res) {
-//    res.send(req.params.id);
-// });
-
-// app.listen(port, () => {
-//    console.log("Starting node.js at port " + port);
-// });
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const datas = require("./data2.json");
+const dataALL = require("./Group/dataALL.json")
+var cors = require('cors')
+app.use(express.json());
+app.use(cors())
+
 
 app.get("/", (req, res) => {
    res.send("DATA REG");
 });
 
 app.get('/datamajor', (req, res) => {
-   res.json(datas);
+   res.json(dataALL);
 });
 
 app.get('/Group/:id', (req, res) => {
    const groupId = req.params.id;
-   const groupData = datas.filter(data => data.group === groupId);
+   try {
+      const data = require("./Group/G" + groupId + ".json")
 
-   if (groupData.length > 0) {
-      res.json(groupData);
-   } else {
+      if (data.length > 0) {
+         res.json(data);
+      } else {
+         res.status(404).send("Group not found");
+      }
+   } catch {
       res.status(404).send("Group not found");
    }
 });
+
+app.get('Filter/day/:id', (req, res) => {
+   const id = req.params.id;
+   try {
+      const data = require("./Group/dataALL.json")
+      if (data.length > 0) {
+         res.json(data);
+      } else {
+         res.status(404).send("Group not found");
+      }
+   } catch {
+      res.status(404).send("Group not found");
+   }
+})
+// "time": "Fr08:00-10:00 IT-507 & We13:00-15:00 IT-507"
+
+app.post('/Filter', (req, res) => {
+   const searchData = req.body;
+   // console.log(searchData.code);
+   const data = require("./Group/dataALL.json")
+   const searchResults = data.filter(item => {
+      return searchData.type === item.type &&
+         searchData.code.includes(item.code) &&
+         searchData.date.includes(item.time.substring(0, 2)) &&
+         (
+            item.time.split(' & ').filter((fitem) => fitem.substring(2, 4) === searchData.time).length > 0 || searchData.time === "total"
+         )
+   });
+   // console.log(searchResults);
+   res.send(searchResults);
+});
+
 
 app.listen(port, () => {
    console.log("Starting node.js at port " + port);
