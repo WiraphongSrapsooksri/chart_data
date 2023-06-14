c3fb21451f9a218bde9f4aae6846b5331d5afd1d
Wiraphong2003 <111116563+Wiraphong2003@users.noreply.github.com>
2023-06-15 04:44:29 +0700

    as

--- index.js
+++ index.js
@@ -31,6 +31,20 @@ app.get('/Group/:id', (req, res) => {
    }
 });
 
+app.get('/seccount/:type', (req, res) => {
+   const type = req.params.type;
+   try {
+      const filteredData = dataALL.filter(item => item.type === type);
+      // const result = filteredData.map(item => ({
+      //    code: item.code,
+      //    name: item.name,
+      //    seccount: item.length
+      // }));
+      res.json(filteredData);
+   } catch {
+      res.status(404).send("not found");
+   }
+});
 
 app.post('/Filter', (req, res) => {
    try {
